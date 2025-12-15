352d73be0c5dbc047bb83103a6364d4c25abe17a
Ayaan Munshi <ayaanmunshi456@gmail.com>
2025-12-14 23:26:39 -0500

    fixed up bio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,8 +11,41 @@ import Image from 'next/image'
 const projects = [
   {
     id: 1,
+    title: 'Rippling',
+    role: 'Software Engineer Intern - Time Tracking Team',
+    date: 'Incoming May 2026 - August 2026',
+    location: 'New York, USA',
+    description:
+      'Incoming Summer 2026',
+    tech: ['Python', 'Django', 'AWS', 'React'],
+    link: 'https://www.rippling.com/about'
+  },
+  {
+    id: 2,
+    title: 'BMO Capital Markets',
+    role: 'Quantitative Trading Intern - Global Markets Team',
+    date: 'Incoming January 2026 - April 2026',
+    location: 'Toronto, Canada',
+    description:
+      'Incoming Winter 2026',
+    tech: ['Python', 'KDB/Q', 'SQL', 'Excel'],
+    link: 'https://capitalmarkets.bmo.com/en/about-us/'
+  },
+  {
+    id: 3,
+    title: 'RBC Capital Markets ',
+    role: 'Quantitative Developer Intern - Quantitative Technology Services Team',
+    date: 'September 2025 - December 2025',
+    location: 'Toronto, Canada',
+    description:
+      'Built out a centralized datastore for a 40+ app ecosystem supporting corporate bankers.',
+    tech: ['C#', 'Python', 'SQL', 'Azure', 'React'],
+    link: 'https://www.rbccm.com/en/about-us'
+  },
+  {
+    id: 4,
     title: 'RBC Capital Markets',
-    role: 'Software Engineer Intern - US Cash Management Team',
+    role: 'Software Engineer Intern - US Cash Management Team (Amplify)',
     date: 'May 2025 - August 2025',
     location: 'Toronto, Canada',
     description:
@@ -21,7 +54,7 @@ const projects = [
     link: 'https://www.rbccm.com/en/expertise/rbcclear.page'
   },
   {
-    id: 2,
+    id: 5,
     title: 'RBC Capital Markets ',
     role: 'Software Engineeer Intern - Enterprise Market Risk Team',
     date: 'May 2024 - August 2024',
@@ -29,12 +62,12 @@ const projects = [
     description:
       'Developed an web service automating delivery of real-time risk analytics',
     tech: ['JavaScript', 'Exchange Web Services', 'Redis', 'Express'],
-    link: 'https://www.rbccm.com/en/'
+    link: 'https://www.rbccm.com/en/about-us'
   },
   {
-    id: 3,
+    id: 6,
     title: 'Beats by Dre',
-    date: 'Feb 2025 - March 2025',
+    date: 'February 2025 - March 2025',
     location: 'California, USA (Remote)',
     role: 'Data Analytics Intern - Consumer Insights Team',
     description:
@@ -44,7 +77,7 @@ const projects = [
     link: 'https://www.beatsbydre.com/ca'
   },
   {
-    id: 4,
+    id: 7,
     title: 'Canam Group',
     role: 'Engineering Intern',
     date: 'May 2023 - August 2023',
@@ -107,21 +140,21 @@ export default function Home() {
       <div className="flex items-center gap-1.5 font-medium tracking-tight">
         <h2 className="text-base text-foreground">engineer</h2>
         <span className="text-foreground/50">•</span>
-        <h2 className="text-base text-foreground">learner</h2>
+        <h2 className="text-base text-foreground">pathfinder</h2>
         <span className="text-foreground/50">•</span>
-        <h2 className="text-base text-foreground">thinker</h2>
+        <h2 className="text-base text-foreground">calisthenic</h2>
       </div>
 
       <p className="pt-4 text-sm text-foreground leading-relaxed">
-        Hi, I’m Ayaan, a software engineer fascinated by how
+        Hi, I’m Ayaan, a software engineer fascinated by how markets
         <span className="text-primary"> markets </span> shape systems, how
         <span className="text-primary"> politics </span> drives incentives, and how
         <span className="text-primary"> technology </span> amplifies both.
         <br /><br />
-        I’ve built and shipped software across startups, corporations, and banks.
-        These days, I’m exploring how ideas spread and why people make the choices they do.
+        I’ve built and shipped software across many different industries.
+
         <br /><br />
-        Outside of work, you’ll find me playing soccer, exploring new cities, or catching up with friends.
+        Outside of work, you’ll find me playing soccer, learning poker, or finding new spots to backpack.
       </p>
 
 
@@ -136,20 +169,20 @@ export default function Home() {
           ))}
         </div>
 
-        <div className="group">
+        {/* <div className="group">
           <p className="mt-4 w-fit rounded-md border border-foreground/10 bg-hoverColor px-2 py-1 text-xs text-foreground transition-all duration-300 ease-in-out">
             Feel free to explore my{' '}
             <Link
-              href="https://github.com/rsalim0?tab=repositories"
+              href="https://github.com/ayaancodes"
               rel="noopener noreferrer"
               target="_blank"
               className="font-medium text-primary underline underline-offset-2 transition-all duration-150 ease-in-out md:hover:text-accent md:group-hover:text-accent"
             >
               GitHub
             </Link>{' '}
-            for more projects. Most of them are open-source.
+            for more projects. 
           </p>
-        </div>
+        </div> */}
       </section>
 
       <section className="pt-8">
